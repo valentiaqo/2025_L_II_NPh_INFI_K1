4aa64ce60c5a07f25c13eb71a3a6218af032ba32
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-04-06 16:36:15 +0200

    Dodanie komend Dockera do Makefile oraz config.yml

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Vlenty"
+moje_imie = "Vlntn"
 msg = "Hello World!"
 
 
